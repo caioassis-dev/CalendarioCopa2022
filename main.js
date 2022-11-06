5b9d3833d29554c3c6613018b2ff8f345bb15340
Caio Freire Lima de Assis <CS376562@MinhaTI.com.br>
2022-11-06 00:30:09 -0300

    refact(calendario_copa/JS):Adicionando novas funcoes para deixar script e aplicacao dinamica.

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ function createGame(player1, hour, player2){
   `
 }
 
-
+let delay = -0.4;
 function createCard(date, day, games) {
+  delay = delay + 0.4;
   return `
-  <div class="card">
+  <div class="card" style="animation-delay: ${delay}s">
   <h2>${date} <span>${day}</span></h2>
   <ul>
     ${games}
@@ -21,12 +22,8 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-  <img src="./assets/Logo_2022.svg.png" alt="Logo da copa" />
-</header>
-<main id="cards">
-  ${createCard("24/11", "quinta", createGame('serbia', '16:00', 'brazil'))}
-  ${createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland'))}
-  ${createCard("02/12", "sexta", createGame('cameroon', '16:00', 'brazil'))}
-</main>`
+document.querySelector("#cards").innerHTML = 
+  createCard("24/11", "quinta", createGame('serbia', '16:00', 'brazil'))+
+  createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland'))+
+  createCard("02/12", "sexta", createGame('cameroon', '16:00', 'brazil'))
+
