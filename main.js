2acc33d8f3130e4284a551e912665578b0aa3db5
Caio Freire Lima de Assis <CS376562@MinhaTI.com.br>
2022-12-02 23:13:09 -0300

    refact(calendarioCopa):Ajustando aplicacao para adicionar placar dos jogos.

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 
-function createGame(player1, hour, player2){
+function createGame(player1, hour, player2, scorePlayer1, scorePlayer2) {
   return `
   <li>
       <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
       <strong>${hour}</strong>
       <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
     </li>
+  <li >
+    <h2 >${scorePlayer1}</h2>
+    <h2>X</h2>
+    <h2>${scorePlayer2}</h2>
+  </li>
   `
 }
 
@@ -23,7 +28,7 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML = 
-  createCard("24/11", "quinta", createGame('serbia', '16:00', 'brazil'))+
-  createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland'))+
-  createCard("02/12", "sexta", createGame('cameroon', '16:00', 'brazil'))
+  createCard("24/11", "quinta", createGame('serbia', '16:00', 'brazil', '0', '2'))+
+  createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland','1', '0'))+
+  createCard("02/12", "sexta", createGame('cameroon', '16:00', 'brazil','1', '0'))
 
